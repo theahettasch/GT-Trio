4a427d15c89f889da0a529bedbbb874577076a16
theahettasch <thea.johanna@hettasch.com>
2026-05-12 10:37:00 +0200

    Add RagTag options

--- scripts/scaffolding.sh
+++ scripts/scaffolding.sh
@@ -9,13 +9,16 @@ reference="$3"
 out_hap1="$4"
 out_hap2="$5"
 threads="$6"
-outdir="$7"
+group_conf="$7"
+loc_conf="$8"
+orient_conf="$9"
+outdir="$10"
 
 mkdir -p ${outdir}/ragtag
 
 #Scaffold contigs agains reference genome
-ragtag.py scaffold ${reference} ${in_hap1} -t ${threads} -i 0.7 -a 0.6 -s 0.7 -o ${outdir}/ragtag/paternal
-ragtag.py scaffold ${reference} ${in_hap2} -t ${threads} -i 0.7 -a 0.6 -s 0.7 -o ${outdir}/ragtag/maternal
+ragtag.py scaffold ${reference} ${in_hap1} -t ${threads} -i ${group_conf} -a ${loc_conf} -s ${orient_conf} -o ${outdir}/ragtag/paternal
+ragtag.py scaffold ${reference} ${in_hap2} -t ${threads} -i ${group_conf} -a ${loc_conf} -s ${orient_conf} -o ${outdir}/ragtag/maternal
 
 #Convert RagTag output to assembly FASTA
 awk '/^>/{gsub(/_RagTag/,"")} {print}' ${outdir}/ragtag/paternal/ragtag.scaffold.fasta > ${out_hap1}
