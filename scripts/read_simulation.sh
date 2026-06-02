ee3d2dc26a27f5208360b933ce944fe6dc8f6fc7
theahettasch <thea.johanna@hettasch.com>
2026-06-02 14:26:23 +0200

    Update read_simulation.sh
    Modify cat command for maternal reads.

--- scripts/read_simulation.sh
+++ scripts/read_simulation.sh
@@ -29,7 +29,7 @@ wgsim -1 $read_length -2 $read_length -N $read_pairs -r $mutation_rate -R $indel
 cat ${tmpdir}/pat_hap1_R1.fastq ${tmpdir}/pat_hap1_R2.fastq ${tmpdir}/pat_hap2_R1.fastq ${tmpdir}/pat_hap2_R2.fastq | gzip > ${out_pat}
 
 #Simulate reads from maternal sequences
-wgsim -1 $read_length -2 $read_length -N $read_pairs -r $mutation_rate -R $indel_fraction -X $indel_extension -e $error_rate ${mat_hap1} ${tmpdir}/mat_hap1_1.fastq ${tmpdir}/mat_hap1_2.fastq
-wgsim -1 $read_length -2 $read_length -N $read_pairs -r $mutation_rate -R $indel_fraction -X $indel_extension -e $error_rate ${mat_hap2} ${tmpdir}/mat_hap2_1.fastq ${tmpdir}/mat_hap2_2.fastq
+wgsim -1 $read_length -2 $read_length -N $read_pairs -r $mutation_rate -R $indel_fraction -X $indel_extension -e $error_rate ${mat_hap1} ${tmpdir}/mat_hap1_R1.fastq ${tmpdir}/mat_hap1_R2.fastq
+wgsim -1 $read_length -2 $read_length -N $read_pairs -r $mutation_rate -R $indel_fraction -X $indel_extension -e $error_rate ${mat_hap2} ${tmpdir}/mat_hap2_R1.fastq ${tmpdir}/mat_hap2_R2.fastq
 
-cat ${mat_hap1} ${tmpdir}/mat_hap1_1.fastq ${tmpdir}/mat_hap1_2.fastq ${tmpdir}/mat_hap2_1.fastq ${tmpdir}/mat_hap2_2.fastq | gzip > ${out_mat}
+cat ${tmpdir}/mat_hap1_R1.fastq ${tmpdir}/mat_hap1_R2.fastq ${tmpdir}/mat_hap2_R1.fastq ${tmpdir}/mat_hap2_R2.fastq | gzip > ${out_mat}
